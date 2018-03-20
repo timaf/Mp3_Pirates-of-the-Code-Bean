789f8e39bc7a9f22bc2ab70c38e9a4e841660809
timaf <lo_sa_na@yahoo.com>
2018-03-20 19:07:14 +0100

    first commit

---
+++ src/main/java/at/refugeescode/Mp3_PiratesoftheCodeBean/Mp3PiratesOfTheCodeBeanApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.Mp3_PiratesoftheCodeBean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp3PiratesOfTheCodeBeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp3PiratesOfTheCodeBeanApplication.class, args);
+	}
+}
